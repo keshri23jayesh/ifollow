f91aa2f4d4b072c79388c4e6deb7ca9b3f6551a5
keshri23jayesh <keshri23.jayesh@gmail.com>
2019-02-06 20:44:38 +0530

    final commmit

--- app/src/main/java/com/example/jayesh123/ifollow/MainActivity.java
+++ app/src/main/java/com/example/jayesh123/ifollow/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.jayesh123.ifollow;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Double lat;
+    Double lng;
+    private BroadcastReceiver broadcastReceiver;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
